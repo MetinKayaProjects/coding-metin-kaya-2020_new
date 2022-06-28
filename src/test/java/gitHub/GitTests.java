358fe3f4c5a15326e8d22e397778a4f6ac09b008
metinkayava <metin.kaya.va@gmail.com>
2022-06-27 21:36:53 -0400

    e

--- src/test/java/gitHub/GitTests.java
+++ src/test/java/gitHub/GitTests.java
@@ -4,5 +4,6 @@ public class GitTests {
     public static void main(String[] args) {
         System.out.println("\"code from gittest_3\" = " + "code from gittest_3");
         System.out.println("code from branch 3");
+        System.out.println("mkyLastBranch");
     }
 }
