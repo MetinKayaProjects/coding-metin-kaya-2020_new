69d988c6b6438c913e068a5a07a37d0fc2dd7d35
metinkayava <metin.kaya.va@gmail.com>
2022-06-27 20:29:27 -0400

    code update in gitTest2

--- src/test/java/gitHub/GitTests.java
+++ src/test/java/gitHub/GitTests.java
@@ -2,6 +2,7 @@ package gitHub;
 
 public class GitTests {
     public static void main(String[] args) {
-        System.out.println("\"code from gittest_1\" = " + "code from gittest_1");
+        System.out.println("\"code from gittest_2\" = " + "code from gittest_2");
+        System.out.println("code from gitTest2");
     }
 }
